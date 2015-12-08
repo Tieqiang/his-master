058c30f4c4298f2e84748c1f9c28327f4b22fc92
wangjing709 <wangjing@jims-cloud.com>
2015-12-08 14:15:50 +0800

    【wj】用户登录姓名密码验证成功后，进入选择医院模块库房页，根据员工编号和医院编码来加载用户拥有权限的系统模块

--- jims-domain/src/main/java/com/jims/his/domain/common/facade/ModuleDictFacade.java
+++ jims-domain/src/main/java/com/jims/his/domain/common/facade/ModuleDictFacade.java
@@ -38,6 +38,24 @@ public class ModuleDictFacade extends BaseFacade {
     }
 
     /**
+     * 根据医院编号和员工编号查询模块列表
+     * @param staffId
+     * @param hospitalId
+     * @return
+     */
+    public List<ModulDict> findByStaff(String staffId, String hospitalId) {
+        String sql = "select a.* from Modul_Dict a,Staff_Vs_Module b where a.id=b.module_Id";
+        if (staffId != null && staffId.trim().length() > 0) {
+            sql += " and b.staff_Id='" + staffId.trim() + "'";
+        }
+        if (null != hospitalId && !hospitalId.trim().equals("")) {
+            sql += " and a.hospital_Id='" + hospitalId + "'";
+        }
+        List<ModulDict> nativeQuery = super.createNativeQuery(sql, new ArrayList<Object>(), ModulDict.class);
+        return nativeQuery;
+    }
+
+    /**
      * 保存模块儿维护信息
      * @param modulDict
      */
