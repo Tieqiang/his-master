693b8130b388ed6a1f6d6dfecf83a00c0d6470fa
wangjing709 <wangjing@jims-cloud.com>
2015-12-10 10:51:20 +0800

    【wj】增删改从分步提交变成统一提交

--- jims-main/src/main/java/com/jims/his/service/ieqm/ExpTenderTypeDictService.java
+++ jims-main/src/main/java/com/jims/his/service/ieqm/ExpTenderTypeDictService.java
@@ -2,12 +2,14 @@ package com.jims.his.service.ieqm;
 
 
 import com.jims.his.common.expection.ErrorException;
+import com.jims.his.domain.common.vo.BeanChangeVo;
 import com.jims.his.domain.ieqm.entity.ExpTenderTypeDict;
 import com.jims.his.domain.ieqm.facade.ExpTenderTypeDictFacade;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,51 +30,32 @@ public class ExpTenderTypeDictService {
     public List<ExpTenderTypeDict> expTenderTypeDictList(@QueryParam("name") String name){
         return expTenderTypeDictFacade.findAll(name);
     }
+
+    /**
+     * 保存增删改
+     *
+     * @param beanChangeVo
+     * @return
+     */
     @POST
-    @Path("add")
-    public Response addExpTenderTypeDict(List<ExpTenderTypeDict> expTenderTypeDicts){
+    @Path("merge")
+    public Response save(BeanChangeVo<ExpTenderTypeDict> beanChangeVo) {
         try {
-            List<ExpTenderTypeDict> dicts = expTenderTypeDictFacade.saveTenderTypeDict(expTenderTypeDicts);
-            return Response.status(Response.Status.OK).entity(dicts).build();
-        }catch (Exception e){
+            List<ExpTenderTypeDict> newUpdateDict = new ArrayList<>();
+            newUpdateDict = expTenderTypeDictFacade.save(beanChangeVo);
+            return Response.status(Response.Status.OK).entity(newUpdateDict).build();
+        } catch (Exception e) {
             ErrorException errorException = new ErrorException();
             errorException.setMessage(e);
-            if(errorException.getErrorMessage().toString().indexOf("最大值")!=-1){
+            if (errorException.getErrorMessage().toString().indexOf("最大值") != -1) {
                 errorException.setErrorMessage("输入数据超过长度！");
-            }else if(errorException.getErrorMessage().toString().indexOf("唯一")!=-1){
+            } else if (errorException.getErrorMessage().toString().indexOf("唯一") != -1) {
                 errorException.setErrorMessage("数据已存在，提交失败！");
+            } else {
+                errorException.setErrorMessage("提交失败！");
             }
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
         }
 
-    }
-    @POST
-    @Path("update")
-    public Response updateExpTenderTypeDict(List<ExpTenderTypeDict> expTenderTypeDicts){
-        try {
-            List<ExpTenderTypeDict> dicts = expTenderTypeDictFacade.updateTenderTypeDict(expTenderTypeDicts);
-            return Response.status(Response.Status.OK).entity(dicts).build();
-        }catch (Exception e){
-            ErrorException errorException = new ErrorException();
-            errorException.setMessage(e);
-            if(errorException.getErrorMessage().toString().indexOf("最大值")!=-1){
-                errorException.setErrorMessage("输入数据超过长度！");
-            }else if(errorException.getErrorMessage().toString().indexOf("唯一")!=-1){
-                errorException.setErrorMessage("数据已存在，提交失败！");
-            }
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
-        }
-    }
-    @POST
-    @Path("delete")
-    public Response saveExpTenderTypeDict(List<ExpTenderTypeDict> expTenderTypeDicts){
-        try {
-            List<ExpTenderTypeDict> dicts = expTenderTypeDictFacade.deleteTenderTypeDict(expTenderTypeDicts);
-            return Response.status(Response.Status.OK).entity(dicts).build();
-        }catch (Exception e){
-            ErrorException errorException = new ErrorException();
-            errorException.setMessage(e);
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
-        }
     }
 }
