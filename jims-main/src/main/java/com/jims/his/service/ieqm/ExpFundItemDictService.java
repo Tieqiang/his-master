43c27943d0b1752ad6f05227ea5e008b4ba0bb86
wangjing709 <wangjing@jims-cloud.com>
2015-12-10 10:27:19 +0800

    【wj】增删改从分步提交变成统一提交

--- jims-main/src/main/java/com/jims/his/service/ieqm/ExpFundItemDictService.java
+++ jims-main/src/main/java/com/jims/his/service/ieqm/ExpFundItemDictService.java
@@ -53,6 +53,8 @@ public class ExpFundItemDictService {
                 errorException.setErrorMessage("输入数据超过长度！");
             } else if (errorException.getErrorMessage().toString().indexOf("唯一") != -1) {
                 errorException.setErrorMessage("数据已存在，提交失败！");
+            }else{
+                errorException.setErrorMessage("提交失败！");
             }
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorException).build();
         }
