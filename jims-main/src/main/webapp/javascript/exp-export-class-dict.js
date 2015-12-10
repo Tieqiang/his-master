f7449c1408a0421dc32650ffe99ae01d03e71d8f
wangjing709 <wangjing@jims-cloud.com>
2015-12-10 10:40:59 +0800

    【wj】增删改从分步提交变成统一提交

--- jims-main/src/main/webapp/javascript/exp-export-class-dict.js
+++ jims-main/src/main/webapp/javascript/exp-export-class-dict.js
@@ -5,7 +5,13 @@
  * 消耗品出库分类字典维护
  */
 $(function(){
-    var editRowIndex ;
+    var editIndex;
+    var stopEdit = function () {
+        if (editIndex || editIndex == 0) {
+            $("#dg").datagrid('endEdit', editIndex);
+            editIndex = undefined;
+        }
+    }
     $("#dg").datagrid({
         title:'消耗品出库分类字典维护',
         fit:true,//让#dg数据创铺满父类容器
@@ -21,64 +27,57 @@ $(function(){
             width:"50%",
             editor:{type:'validatebox',options:{
                 required:true,validType:'length[0,8]',missingMessage:'请输入四个以内的汉字'}
-            }}]]
+            }}]],
+        onClickRow: function (index, row) {
+            stopEdit();
+            $(this).datagrid('beginEdit', index);
+            editIndex = index;
+        }
     })  ;
 
+    $("#searchBtn").on("click", function () {
+        loadDict();
+    });
 
     $("#addBtn").on('click',function(){
-
-        $("#dg").datagrid('appendRow',{}) ;
-        var rows = $("#dg").datagrid('getRows') ;
-        var row = rows[rows.length - 1 ] ;
-        var index = $("#dg").datagrid('getRowIndex',row) ;
-
-        $("#dg").datagrid('selectRow',index) ;
-        if(editRowIndex ==index){
-            $("#dg").datagrid('beginEdit',editRowIndex);
-        }
-        if(editRowIndex==undefined){
-            $("#dg").datagrid('beginEdit',index);
-            editRowIndex = index ;
-        }else{
-            $("#dg").datagrid('endEdit',editRowIndex) ;
-            $("#dg").datagrid('beginEdit',index) ;
-            editRowIndex = index ;
-        }
+        stopEdit();
+        $("#dg").datagrid('appendRow', {});
+        var rows = $("#dg").datagrid('getRows');
+        var addRowIndex = $("#dg").datagrid('getRowIndex', rows[rows.length - 1]);
+        editIndex = addRowIndex;
+        $("#dg").datagrid('selectRow', editIndex);
+        $("#dg").datagrid('beginEdit', editIndex);
     }) ;
 
     $("#delBtn").on('click',function(){
-        var row = $("#dg").datagrid('getSelected') ;
-        if(!row){
-            $.messager.alert("系统提醒","请选择要删除的行","error") ;
-            return ;
+        var row = $("#dg").datagrid('getSelected');
+        if (row) {
+            var rowIndex = $("#dg").datagrid('getRowIndex', row);
+            $("#dg").datagrid('deleteRow', rowIndex);
+            if (editIndex == rowIndex) {
+                editIndex = undefined;
+            }
+        } else {
+            $.messager.alert('系统提示', "请选择要删除的行", 'info');
         }
-
-        var index = $("#dg").datagrid('getRowIndex',row) ;
-
-        if(index==editRowIndex){
-            editRowIndex =undefined ;
-        }
-        $("#dg").datagrid('deleteRow',index) ;
-
     }) ;
 
     $("#editBtn").on('click',function(){
-        var row = $("#dg").datagrid('getSelected') ;
-        if(!row){
-            $.messager.alert("系统提醒","请选择要编辑的行","error") ;
-            return ;
+        var row = $("#dg").datagrid("getSelected");
+        var index = $("#dg").datagrid("getRowIndex", row);
+
+        if (index == -1) {
+            $.messager.alert("提示", "请选择要修改的行！", "info");
+            return;
         }
 
-        var index = $("#dg").datagrid('getRowIndex',row) ;
-
-        if(editRowIndex ==undefined){
-
-            $("#dg").datagrid("beginEdit",index) ;
-            editRowIndex = index ;
-        }else{
-            $("#dg").datagrid('endEdit',editRowIndex) ;
-            $("#dg").datagrid('beginEdit',index) ;
-            editRowIndex = index ;
+        if (editIndex == undefined) {
+            $("#dg").datagrid("beginEdit", index);
+            editIndex = index;
+        } else {
+            $("#dg").datagrid("endEdit", editIndex);
+            $("#dg").datagrid("beginEdit", index);
+            editIndex = index;
         }
     }) ;
 
@@ -98,54 +97,27 @@ $(function(){
      * 基础字典的维护只能在基础数据维护的时候使用。
      */
     $("#saveBtn").on('click',function(){
-        if(editRowIndex){
-            $("#dg").datagrid('endEdit',editRowIndex) ;
-            editRowIndex = undefined ;
-        }
-        //var rows = $("#dg").datagrid('getRows') ;
-        //for(var i= 0;i<rows.length;i++){
-        //    for(var j= 1;i<rows.length;j++){
-        //        if(rows[i].exportClass==rows[j].exportClass){
-        //            $.messager.alert('系统提示','类别名称不允许重复',"info");
-        //            return;
-        //        }
-        //    }
-        //}
-        var insertData = $("#dg").datagrid('getChanges','inserted') ;
-        var updateData = $("#dg").datagrid('getChanges','updated') ;
-        var deleteData = $("#dg").datagrid('getChanges','deleted') ;
-
-
-        if(insertData&&insertData.length > 0 ){
-            $.postJSON("/api/exp-export-class-dict/add",insertData,function(data){
-                $.messager.alert('系统提示','保存成功',"info");
-                loadDict() ;
-            },function(data){
-                console.log(data) ;
-                $.messager.alert("系统提示",data.responseJSON.errorMessage,"error");
-                loadDict() ;
-            }) ;
+        if (editIndex || editIndex == 0) {
+            $("#dg").datagrid("endEdit", editIndex);
         }
 
-        if(updateData&&updateData.length > 0 ){
-            $.postJSON("/api/exp-export-class-dict/add",updateData,function(data){
-                $.messager.alert('系统提示','修改成功',"info");
-                loadDict() ;
-            },function(data){
-                console.log(data) ;
-                $.messager.alert("系统提示",data.responseJSON.errorMessage,"error");
-                loadDict() ;
-            }) ;
-        }
-        if(deleteData&&deleteData.length > 0 ){
-            $.postJSON("/api/exp-export-class-dict/delete",deleteData,function(data){
-                $.messager.alert('系统提示','删除成功',"info");
-                loadDict() ;
-            },function(data){
-                console.log(data) ;
-                $.messager.alert("系统提示",data.responseJSON.errorMessage,"error");
-                loadDict() ;
-            }) ;
+        var insertData = $("#dg").datagrid("getChanges", "inserted");
+        var updateDate = $("#dg").datagrid("getChanges", "updated");
+        var deleteDate = $("#dg").datagrid("getChanges", "deleted");
+
+        var beanChangeVo = {};
+        beanChangeVo.inserted = insertData;
+        beanChangeVo.deleted = deleteDate;
+        beanChangeVo.updated = updateDate;
+
+
+        if (beanChangeVo) {
+            $.postJSON("/api/exp-export-class-dict/merge", beanChangeVo, function (data, status) {
+                $.messager.alert("系统提示", "保存成功", "info");
+                loadDict();
+            }, function (data) {
+                $.messager.alert('提示', data.responseJSON.errorMessage, "error");
+            })
         }
     }) ;
 })
