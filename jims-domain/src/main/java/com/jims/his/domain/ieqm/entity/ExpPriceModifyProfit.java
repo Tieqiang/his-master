5c90a73837c68ca0fdc74fee176bc95b3187e58f
jimswbb <1032611539@qq.com>
2015-12-15 09:15:43 +0800

    [wbb] 1,

--- jims-domain/src/main/java/com/jims/his/domain/ieqm/entity/ExpPriceModifyProfit.java
+++ jims-domain/src/main/java/com/jims/his/domain/ieqm/entity/ExpPriceModifyProfit.java
@@ -1,14 +1,8 @@
 package com.jims.his.domain.ieqm.entity;
 
 import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
+
 import org.hibernate.annotations.GenericGenerator;
 
 /**
