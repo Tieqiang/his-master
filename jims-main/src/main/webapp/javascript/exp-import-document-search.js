c2bd9004fc6c63f13438884259df843abe9a810b
wangjing709 <wangjing@jims-cloud.com>
2015-12-16 13:15:50 +0800

    【wj】调整页面显示

--- jims-main/src/main/webapp/javascript/exp-import-document-search.js
+++ jims-main/src/main/webapp/javascript/exp-import-document-search.js
@@ -83,17 +83,17 @@ $(function () {
             title: '库房',
             field: 'storage',
             width: '9%',
+            hidden:true,
             editor: {type: 'textbox'}
-        }, {
-            title: '入库日期',
-            field: 'importDate',
-            width: '9%'
-
         }, {
             title: '入库单号',
             field: 'documentNo',
             width: '9%',
             editor: {type: 'textbox'}
+        }, {
+            title: '入库日期',
+            field: 'importDate',
+            width: '9%'
         }, {
             title: '供货商',
             field: 'supplier',
