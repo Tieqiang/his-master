09ee5fe2ad9f77257cc32ad67f4b94c149841600
wangjing709 <wangjing@jims-cloud.com>
2015-12-18 10:36:08 +0800

    【wj】增删改从分步变成一步提交

--- jims-domain/src/main/java/com/jims/his/domain/ieqm/facade/ExpStorageProfileFacade.java
+++ jims-domain/src/main/java/com/jims/his/domain/ieqm/facade/ExpStorageProfileFacade.java
@@ -2,6 +2,7 @@ package com.jims.his.domain.ieqm.facade;
 
 import com.google.inject.persist.Transactional;
 import com.jims.his.common.BaseFacade;
+import com.jims.his.domain.common.vo.BeanChangeVo;
 import com.jims.his.domain.ieqm.entity.ExpExportClassDict;
 import com.jims.his.domain.ieqm.entity.ExpFormDict;
 import com.jims.his.domain.ieqm.entity.ExpStorageProfile;
@@ -25,66 +26,43 @@ public class ExpStorageProfileFacade extends BaseFacade {
         this.entityManager = entityManager;
     }
 
-    //查询库存
-    //public List<ExpStockDefineVo> findExpStockDefineByFormDict(){
-    //    String hql = "  SELECT a.storage," +
-    //            "         b.expName," +
-    //            "         a.expCode, " +
-    //            "         a.expSpec," +
-    //            "         a.units," +
-    //            "         a.amountPerPackage," +
-    //            "         a.packageUnits," +
-    //            "         a.upperLevel," +
-    //            "         a.lowLevel," +
-    //            "         a.location" +
-    //            "    FROM ExpDict a," +
-    //            "         ExpStorageProfile b" +
-    //            "   WHERE a.expCode = b.expCode and " +
-    //            "a.expSpec = b.expSpec and " +
-    //            "a.storage = '1502' and " +
-    //            "a.expCode like '%' || '1'||'%'";
-    //    return entityManager.createQuery(hql).getResultList();
-    //}
+
     //保存
     @Transactional
-    public List<ExpStockDefineVo> saveExpStockDefine(List<ExpStockDefineVo> insertData){
-        List<ExpStockDefineVo> expStockDefineVoList = new ArrayList<>();
+    public List<ExpStorageProfile> saveExpStockDefine(BeanChangeVo<ExpStorageProfile> beanChangeVo){
+        List<ExpStorageProfile> newUpdateDict = new ArrayList<>();
+        List<ExpStorageProfile> insertData = beanChangeVo.getInserted();
+        List<ExpStorageProfile> updateData = beanChangeVo.getUpdated();
+        List<ExpStorageProfile> deleteData = beanChangeVo.getDeleted();
         if (insertData.size() > 0 ){
-            for (ExpStockDefineVo expStockDefineVo : insertData){
-                ExpStorageProfile expStorageProfile = new ExpStorageProfile();
-
-                expStorageProfile.setAmountPerPackage(expStockDefineVo.getAmountPerPackage());
-                expStorageProfile.setExpCode(expStockDefineVo.getExpCode());
-                expStorageProfile.setExpSpec(expStockDefineVo.getExpSpec());
-                expStorageProfile.setLocation(expStockDefineVo.getLocation());
-                expStorageProfile.setLowLevel(expStockDefineVo.getLowLevel());
-                expStorageProfile.setPackageUnits(expStockDefineVo.getPackageUnits());
-                expStorageProfile.setUpperLevel(expStockDefineVo.getUpperLevel());
-                expStorageProfile.setStorage(expStockDefineVo.getStorage());
-                expStorageProfile.setUnits(expStockDefineVo.getUnits());
-
-                ExpStorageProfile expStorageProfile1 =merge(expStorageProfile);
-
-                expStockDefineVo.setAmountPerPackage(expStorageProfile1.getAmountPerPackage());
-                expStockDefineVo.setExpCode(expStorageProfile1.getExpCode());
-                expStockDefineVo.setExpSpec(expStorageProfile1.getExpSpec());
-                expStockDefineVo.setLocation(expStorageProfile1.getLocation());
-                expStockDefineVo.setLowLevel(expStorageProfile1.getLowLevel());
-                expStockDefineVo.setPackageUnits(expStorageProfile1.getPackageUnits());
-                expStockDefineVo.setUpperLevel(expStorageProfile1.getUpperLevel());
-                expStockDefineVo.setStorage(expStorageProfile1.getStorage());
-                expStockDefineVo.setUnits(expStorageProfile1.getUnits());
-
-                expStockDefineVoList.add(expStockDefineVo);
+            for (ExpStorageProfile dict : insertData){
+                ExpStorageProfile expStorageProfile = merge(dict);
+                newUpdateDict.add(expStorageProfile);
             }
         }
-        return expStockDefineVoList;
+        if(updateData.size() > 0){
+            for (ExpStorageProfile dict : updateData) {
+                ExpStorageProfile expStorageProfile = merge(dict);
+                newUpdateDict.add(expStorageProfile);
+            }
+
+        }
+
+        List<String> ids = new ArrayList<>();
+
+        for (ExpStorageProfile dict : deleteData) {
+            ids.add(dict.getId());
+            newUpdateDict.add(dict);
+        }
+        this.removeByStringIds(ExpStorageProfile.class, ids);
+        return newUpdateDict;
     }
+
     @Transactional
     public List<ExpStorageProfileVo> saveStorageUpdate(List<ExpStorageProfileVo> updateData) {
-        List<ExpStorageProfileVo> newUpdateDict = new ArrayList<>() ;
-        if(updateData.size()>0){
-            for (ExpStorageProfileVo dict:updateData){
+        List<ExpStorageProfileVo> newUpdateDict = new ArrayList<>();
+        if (updateData.size() > 0) {
+            for (ExpStorageProfileVo dict : updateData) {
                 String id = dict.getId();
                 System.out.println(id);
                 ExpStorageProfile expStorageProfile = get(ExpStorageProfile.class, id);
@@ -94,13 +72,14 @@ public class ExpStorageProfileFacade extends BaseFacade {
             }
             newUpdateDict.addAll(updateData);
         }
-        return newUpdateDict ;
+        return newUpdateDict;
     }
 
-    public List<ExpStockDefineVo> findExpStockDefine(String expForm, String expCode,String storage) {
-        String sql = "\n" +
-                "  SELECT EXP_STORAGE_PROFILE.STORAGE,   \n" +
+    public List<ExpStorageProfile> findExpStockDefine(String expForm, String expCode,String storage) {
+        String sql = "SELECT EXP_STORAGE_PROFILE.ID," +
+                "         EXP_STORAGE_PROFILE.STORAGE,   \n" +
                 "         EXP_DICT.EXP_NAME,   \n" +
+                "         EXP_DICT.EXP_FORM,   \n" +
                 "         EXP_STORAGE_PROFILE.EXP_CODE,   \n" +
                 "         EXP_STORAGE_PROFILE.EXP_SPEC,   \n" +
                 "         EXP_STORAGE_PROFILE.UNITS,   \n" +
@@ -114,13 +93,13 @@ public class ExpStorageProfileFacade extends BaseFacade {
                 "   WHERE EXP_STORAGE_PROFILE.EXP_CODE = EXP_DICT.EXP_CODE AND \n" +
                 "\t\t\tEXP_STORAGE_PROFILE.EXP_SPEC = EXP_DICT.EXP_SPEC AND\n" +
                 "\t\t\tEXP_STORAGE_PROFILE.STORAGE = '"+storage+"'   ";
-        if (null != expForm && !expForm.trim().equals("" ) ) {
+        if (null != expForm && !expForm.trim().equals("全部") ) {
             sql += " AND EXP_DICT.exp_form like '%'||'"+expForm+"'||'%'  ";
         }
-        if (null != expCode && !expCode.trim().equals("" ) ) {
+        if (null != expCode && !expCode.trim().equals("") ) {
             sql += " AND EXP_STORAGE_PROFILE.exp_code = '"+expCode+"' ";
         }
-        List<ExpStockDefineVo> nativeQuery = super.createNativeQuery(sql, new ArrayList<Object>(), ExpStockDefineVo.class);
+        List<ExpStorageProfile> nativeQuery = super.createNativeQuery(sql, new ArrayList<Object>(), ExpStorageProfile.class);
         return nativeQuery;
     }
 }
