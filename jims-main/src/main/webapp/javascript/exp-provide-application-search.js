973e25d2b272a11d8035c68c9d4872307c3a21ed
wangjing709 <wangjing@jims-cloud.com>
2015-12-24 14:28:31 +0800

    【wj】查询时间段起止日期从年月日增加时分秒

--- jims-main/src/main/webapp/javascript/exp-provide-application-search.js
+++ jims-main/src/main/webapp/javascript/exp-provide-application-search.js
@@ -19,22 +19,52 @@ $(document).ready(function () {
         })
     });
     //格式化日期(方法)
-    var formatterDate = function (date) {
-        var y = date.getFullYear();
-        var m = date.getMonth() + 1;
-        var d = date.getDate();
-        return y + "-" + (m < 10 ? ("0" + m) : m) + "-" + (d < 10 ? ("0" + d) : d);
-    };
-    var formatterDateBeforeThreeDay = function (date) {
-        var y = date.getFullYear();
-        var m = date.getMonth() + 1;
-        var d = date.getDate() - 3;
-        return y + "-" + (m < 10 ? ("0" + m) : m) + "-" + (d < 10 ? ("0" + d) : d);
-    };
+    function formatterDate(val, row) {
+        if (val != null) {
+            var date = new Date(val);
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            var d = date.getDate();
+            var h = date.getHours();
+            var mm = date.getMinutes();
+            var s = date.getSeconds();
+            var dateTime = y + "-" + (m < 10 ? ("0" + m) : m) + "-" + (d < 10 ? ("0" + d) : d) + ' '
+                + (h < 10 ? ("0" + h) : h) + ":" + (mm < 10 ? ("0" + mm) : mm) + ":" + (s < 10 ? ("0" + s) : s);
+            return dateTime
+        }
+    }
 
-    //设置默认值
-    $("#startTime").datebox("setValue", formatterDateBeforeThreeDay(new Date()));//提前3天
-    $("#endTime").datebox("setValue", formatterDate(new Date()));
+    $('#startTime').datetimebox({
+        required: true,
+        showSeconds: true,
+        value: 'dateTime',
+        formatter: formatterDate,
+        onSelect: function (date) {
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            var d = date.getDate();
+            var time = $('#startTime').datetimebox('spinner').spinner('getValue');
+            var dateTime = y + "-" + (m < 10 ? ("0" + m) : m) + "-" + (d < 10 ? ("0" + d) : d) + ' ' + time;
+            $('#startTime').datetimebox('setText', dateTime);
+            $('#startTime').datetimebox('hidePanel');
+        }
+    });
+    $('#endTime').datetimebox({
+        value: 'dateTime',
+        required: true,
+        showSeconds: true,
+        formatter: formatterDate,
+        onSelect: function (date) {
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            var d = date.getDate();
+            var time = $('#endTime').datetimebox('spinner').spinner('getValue');
+            var dateTime = y + "-" + (m < 10 ? ("0" + m) : m) + "-" + (d < 10 ? ("0" + d) : d) + ' ' + time;
+            //(m < 10 ? ("0" + m) : m) + "/" + (d < 10 ? ("0" + d) : d) + "/" + y + " " + time;
+            $('#endTime').datetimebox('setText', dateTime);
+            $('#endTime').datetimebox('hidePanel');
+        }
+    });
 
 
     $("#dg").datagrid({
@@ -71,31 +101,31 @@ $(document).ready(function () {
         }, {
             title: "单位",
             field: "packageUnits",
-            width: "10%"
+            width: "7%"
         }, {
             title: "数量",
             field: "quantity",
-            width: "10%"
+            width: "7%"
         }, {
             title: "申请人",
             field: "applicationMan",
             width: "10%"
+        }, {
+            title: "申请时间",
+            field: "enterDateTime",
+            width: "11%",
+            formatter: formatterDate
         }]]
     });
     //查询
     $("#searchBtn").on("click", function () {
-        var startTimeStr = $("#startTime").datebox("getValue").split("-");
-        var startTime = startTimeStr[0]+"/"+startTimeStr[1]+"/"+startTimeStr[2];
-        //alert(startTime);
-        var endTimeStr = $("#endTime").datebox("getValue").split("-");
-        var endTime = endTimeStr[0]+"/"+endTimeStr[1]+"/"+endTimeStr[2];
+        var startTime = $("#startTime").datetimebox('getText');
+        var endTime = $('#endTime').datetimebox('getText');
 
         var isSure = $("#topBar input[name='radioOne']:checked").val();
         var applicationStorage = parent.config.storageCode;
-        //alert("startTime:"+startTime+"  endTime:"+endTime+"  isSure:"+isSure);
 
         var expProvideApplicationObjs = [];
-
 
         $.get("/api/exp-provide-application/find-cur-storage-application" +
             "?startTime=" + startTime + "&endTime=" + endTime + "&isSure=" + isSure + "&applicationStorage=" + applicationStorage, function (data) {
