ebeecbac047216cd8d95d16d9bbc6819ef784ea4
chenxy <15101128707@163.com>
2016-05-28 14:01:44 +0800

    [cxy]  出库记录查询----显示代码和查询逻辑错误完毕。

--- jims-main/src/main/webapp/javascript/ieqm/exp-export-record-search.js
+++ jims-main/src/main/webapp/javascript/ieqm/exp-export-record-search.js
@@ -70,10 +70,10 @@ function myFormatter2(val,row) {
         var y = date.getFullYear();
         var m = date.getMonth()+1;
         var d = date.getDate();
-        var h = date.getHours();
-        var min = date.getMinutes();
-        var sec = date.getSeconds();
-        var str = y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d)+' '+(h<10?('0'+h):h)+':'+(min<10?('0'+min):min)+':'+(sec<10?('0'+sec):sec);
+//        var h = date.getHours();
+//        var min = date.getMinutes();
+//        var sec = date.getSeconds();
+        var str = y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d);/*+' '+(h<10?('0'+h):h)+':'+(min<10?('0'+min):min)+':'+(sec<10?('0'+sec):sec);*/
         return str;
     }
 }
