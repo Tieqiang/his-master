d33a8b7840bf4ffea007e8ab8c60a09ec250116a
fyg408224345 <408224345@qq.com>
2016-05-28 10:41:38 +0800

    [冯宇光]: 消耗品基础字典维护:添加多条数据失败问题；追加键值时让字典名称不可编辑；选中任意一条数据追加键值。 base-dict.js

--- jims-main/src/main/webapp/javascript/common/base-dict.js
+++ jims-main/src/main/webapp/javascript/common/base-dict.js
@@ -52,7 +52,6 @@ $(function(){
 
     var loadDict = function () {
         //var name = $("#name").textbox("getValue");
-
         $.get('/api/base-dict/list-by-type', function (data) {
             $("#baseDictDg").datagrid('loadData', data);
         });
@@ -79,29 +78,59 @@ $(function(){
      * 追加键值
      */
     $("#appendBaseDictBtn").on('click',function(){
-        stopEdit() ;
-        var rows =$("#baseDictDg").datagrid('getRows') ;
-        var lastRow ;
-        if(rows.length>0){
-            lastRow = rows[rows.length - 1] ;
-        }
+        //选中某行，在某行后面追加键值
+        if(typeof (editIndex) != "undefined"){
+            $("#baseDictDg").datagrid('endEdit', editIndex);
+            var thisRow = $("#baseDictDg").datagrid('getSelected');
+            editIndex = editIndex + 1 ;
+            $("#baseDictDg").datagrid('insertRow',{
+                index: editIndex ,
+                row: {
+                    id: '',
+                    baseCode: '',
+                    baseName: '',
+                    baseType: thisRow.baseType
+                }
+            });
+            $("#baseDictDg").datagrid('selectRow', editIndex);
+            $("#baseDictDg").datagrid('beginEdit',editIndex);
+            var dateEd = $("#baseDictDg").datagrid('getEditor', {index: editIndex, field: 'baseType'});
+            //$(dateEd.target).attr('disabled',true); //设置字典名称不可编辑
+            var col = dateEd.target;
+            col.prop('readonly',true);   //设置字典名称只读
+        }else{      //未选中任意一行，直接在最后一行追加键值
+            stopEdit();
+            var rows = $("#baseDictDg").datagrid('getRows');
+            var lastRow;
+            if (rows.length > 0) {
+                lastRow = rows[rows.length - 1];
+            }
 
-        if(lastRow){
-            $("#baseDictDg").datagrid('appendRow',{id:'',baseCode:'',baseName:'',baseType:lastRow.baseType}) ;
-            var newRows = $("#baseDictDg").datagrid('getRows') ;
-            var newRowIndex = $("#baseDictDg").datagrid('getRowIndex',newRows[newRows.length -1 ]) ;
+            if (lastRow) {
+                $("#baseDictDg").datagrid('appendRow', {id: '', baseCode: '', baseName: '', baseType: lastRow.baseType});
+                var newRows = $("#baseDictDg").datagrid('getRows');
+                var newRowIndex = $("#baseDictDg").datagrid('getRowIndex', newRows[newRows.length - 1]);
 
-            editIndex = newRowIndex ;
-            $("#baseDictDg").datagrid('selectRow',editIndex) ;
-            $("#baseDictDg").datagrid('beginEdit',editIndex) ;
-        }else{
-            $("#baseDictDg").datagrid('appendRow',{}) ;
-            var newRows = $("#baseDictDg").datagrid('getRows') ;
-            var newRowIndex = $("#baseDictDg").datagrid('getRowIndex',newRows[newRows.length -1 ]) ;
+                editIndex = newRowIndex;
+                $("#baseDictDg").datagrid('selectRow', editIndex);
+                $("#baseDictDg").datagrid('beginEdit', editIndex);
+                var dateEd = $("#baseDictDg").datagrid('getEditor', {index: editIndex, field: 'baseType'});
+                //$(dateEd.target).attr('disabled',true); //设置字典名称不可编辑
+                var col = dateEd.target;
+                col.prop('readonly', true);   //设置字典名称只读
+            } else {
+                $("#baseDictDg").datagrid('appendRow', {});
+                var newRows = $("#baseDictDg").datagrid('getRows');
+                var newRowIndex = $("#baseDictDg").datagrid('getRowIndex', newRows[newRows.length - 1]);
 
-            editIndex = newRowIndex ;
-            $("#baseDictDg").datagrid('selectRow',editIndex) ;
-            $("#baseDictDg").datagrid('beginEdit',editIndex) ;
+                editIndex = newRowIndex;
+                $("#baseDictDg").datagrid('selectRow', editIndex);
+                $("#baseDictDg").datagrid('beginEdit', editIndex);
+                var dateEd = $("#baseDictDg").datagrid('getEditor', {index: editIndex, field: 'baseType'});
+                //$(dateEd.target).attr('disabled',true); //设置字典名称不可编辑
+                var col = dateEd.target;
+                col.prop('readonly', true);   //设置字典名称只读
+            }
         }
     }) ;
 
@@ -109,7 +138,6 @@ $(function(){
      * 删除字典操作
      */
     $("#delBaseDictBtn").on('click',function(){
-
         var row = $("#baseDictDg").datagrid('getSelected') ;
         if(row){
             var rowIndex = $("#baseDictDg").datagrid('getRowIndex',row) ;
@@ -117,13 +145,9 @@ $(function(){
             if(editIndex ==rowIndex){
                 editIndex = undefined ;
             }
-
         }else{
             $.messager.alert('系统提示',"请选择要删除的行",'info') ;
         }
-
-
-
     }) ;
 
     /**
@@ -143,7 +167,6 @@ $(function(){
      * 保存修改内容
      */
     $("#saveBaseDictBtn").on('click',function(){
-
         if(editIndex||editIndex==0){
             $("#baseDictDg").datagrid('endEdit',editIndex) ;
 
@@ -203,7 +226,6 @@ $(function(){
             $.messager.alert("系统提示","保存失败","error");
             loadDict();
         })
-
     }) ;
 
 })
