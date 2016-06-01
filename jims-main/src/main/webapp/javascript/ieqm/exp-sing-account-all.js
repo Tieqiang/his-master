9fda73f77fb486883c56089c729b0673108da8a0
wei <742041253@qq.com>
2016-06-01 09:25:30 +0800

    魏申 修改查询结果与数据库不一致

--- jims-main/src/main/webapp/javascript/ieqm/exp-sing-account-all.js
+++ jims-main/src/main/webapp/javascript/ieqm/exp-sing-account-all.js
@@ -191,54 +191,32 @@ $(function () {
     var importDetailDataVO = {};//传递vo
     var detailsData = [];//信息
     var specCodeAll =[];
-    var data2="";
     var loadDict = function(){
         importDetailDataVO.stopDate = new Date($("#stopDate").datetimebox("getText"));
         importDetailDataVO.startDate = new Date($("#startDate").datetimebox("getText"));
         importDetailDataVO.hospitalId = parent.config.hospitalId;
         importDetailDataVO.storage = parent.config.storageCode;
         var promise =$.get("/api/exp-import/exp-single-account",importDetailDataVO,function(data){
-            console.log("data"+data.length)
-            if(data.length<=0){
 
+            if(data.length<=0){
                 $.messager.alert('系统提示','数据库暂无数据','info');
                 $("#importDetail").datagrid('loadData',[]);
                 return;
             }
-            console.log(specCodeAll)
             var specCode ={};
             var i = 0;
-            //if(specCodeAll.length<=0){
-            //    specCode.expCode=data[i].expCode;
-            //    specCode.expName=data[i].expName;
-            //    specCode.packageSpec=data[i].packageSpec;
-            //    specCodeAll.push(specCode);
-            //}
-            //if(specCodeAll.length>0){
-            //    for(var q=0;q<data.length;q++){
-            //        for(var w=0 ;w<specCodeAll.length;w++){
-            //            if(data[q].expCode!=specCodeAll[w].expCode &&data[q].packageSpec!=specCodeAll[w].packageSpec&&data[q].expName!=specCodeAll[w].expName){
-            //                specCode.expCode=data[q].expCode;
-            //                specCode.packageSpec=data[q].packageSpec;
-            //                specCodeAll.push(specCode);
-            //            }
-            //        }
-            //    }
-            //}
-            //console.log(specCodeAll.length);
             if(specCodeAll.length<=0 ){
                 var importPrice = 0.00;
                 var exportPrice = 0.00;
                 specCode.expCode=data[i].expCode;
                 specCode.packageSpec=data[i].packageSpec;
                 specCodeAll.push(specCode);
-                console.log("第一次"+specCodeAll)
                 for(var l = 0 ;l<data.length;l++){
                     if(data[l].expCode==specCode.expCode && data[l].packageSpec==specCode.packageSpec){
                         importPrice+=data[l].importPrice;
                         exportPrice+=data[l].exportPrice;
                         detailsData.push(data[l]);
-                        console.log("+++++++"+l)
+
                     }
                 }
                 var sp = {};
@@ -252,38 +230,29 @@ $(function () {
                 var exportPrice = 0.00;
                 for(var m=0;m<data.length;m++){
                     for(var j =0;j<specCodeAll.length;j++){
-                        //console.log(specCodeAll[j].expCode);
-                        //console.log(specCodeAll[j].packageSpec);
                         if(data[m].expCode!=specCodeAll[j].expCode && data[m].packageSpec!=specCodeAll[j].packageSpec){
                             specCode.expCode=data[m].expCode;
                             specCode.packageSpec=data[m].packageSpec;
                             specCodeAll.push(specCode);
-                            console.log("m次----"+m)
-                            //console.log(specCodeAll+"1");
-                            //console.log(specCodeAll);
+
                             for(var l = 0 ;l<data.length;l++){
-                                console.log("datad长度"+data)
+
                                if(data[l].expCode==specCodeAll[specCodeAll.length-1].expCode && data[l].packageSpec==specCodeAll[specCodeAll.length-1].packageSpec){
                                     importPrice+=data[l].importPrice;
                                     exportPrice+=data[l].exportPrice;
-                                    console.log(importPrice);
-                                    console.log(exportPrice);
-                                    //detailsData.push(data[l]);
-                                    console.log("第二次"+specCodeAll)
-                                    //console.log(detailsData);
-                                    //console.log("huan");
-                                    console.log("------"+l)
-                                }
+                                    detailsData.push(data[l]);
+
+                                }else if(data[l].expCode==specCodeAll[specCodeAll.length-1].expCode && data[l].packageSpec!=specCodeAll[specCodeAll.length-1].packageSpec){
+                                   importPrice+=data[l].importPrice;
+                                   exportPrice+=data[l].exportPrice;
+                                   detailsData.push(data[l]);
+                               }
                             }
                             var sp = {};
                             sp.ioClass= "单品总合计：";
                             sp.importPrice = importPrice;
                             sp.exportPrice =exportPrice;
-                            //detailsData.push(data)
-                            //detailsData.push(sp);
-                            
-                            data.push(sp);
-                            data2=data;
+                            detailsData.push(sp);
                             var importPrice = 0.00;
                             var exportPrice = 0.00;
                          }
@@ -294,11 +263,11 @@ $(function () {
             specCodeAll.splice(0,specCodeAll.length);
         },'json');
         promise.done(function(){
-            $("#importDetail").datagrid('loadData',data2);
-        })
+            $("#importDetail").datagrid('loadData',detailsData);
+        });
         detailsData.splice(0,detailsData.length);
 
 
 
     }
-})
+});
