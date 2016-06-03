67f09df6d8ca260e10e7cb6c6441eb24cf56c88c
zhu9644 <964483087@qq.com>
2016-06-03 16:43:02 +0800

    [朱齐]增加 根据条码查询 接口

--- jims-domain/src/main/java/com/jims/his/domain/ieqm/facade/ExpPrepareDetailFacade.java
+++ jims-domain/src/main/java/com/jims/his/domain/ieqm/facade/ExpPrepareDetailFacade.java
@@ -1,9 +1,12 @@
 package com.jims.his.domain.ieqm.facade;
 
 import com.jims.his.common.BaseFacade;
+import com.jims.his.domain.ieqm.vo.ExpPrepareVo;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/6/2.
@@ -15,6 +18,30 @@ public class ExpPrepareDetailFacade extends BaseFacade {
     @Inject
     public ExpPrepareDetailFacade (EntityManager entityManager){
         this.entityManager=entityManager;
+    }
+
+    public List<ExpPrepareVo>findByBarCode(String barCode){
+        String sql="select a.use_flag,a.master_id , \n"+
+                "b.exp_id,b.prepare_date,b.prepare_person_name,b.phone,b.price,  \n"+
+                "c.exp_name,c.exp_spec,c.units,  \n"+
+                "d.supplier  \n"+
+                "from exp_prepare_detail a,exp_prepare_master b,exp_dict c ,exp_supplier_catalog d  \n"+
+                "where a.master_id=b.id and b.exp_id=c.exp_code and b.supplier_id=d.supplier_id \n"+
+                "and a.exp_bar_code='"+barCode+"'";
+        List<ExpPrepareVo> list=super.createNativeQuery(sql,new ArrayList<Object>(),ExpPrepareVo.class);
+        if(list.size()==0){
+            return null;
+        }else{
+            for(int i=0;i<list.size();i++){
+                String a=list.get(i).getUseFlag();
+                if(a.equals("0")){
+                    list.get(i).setUseFlag("未使用");
+                }else{
+                    list.get(i).setUseFlag("已使用");
+                }
+            }
+            return list;
+        }
     }
 }
 
--- jims-main/src/main/java/com/jims/his/service/ieqm/ExpPrepareService.java
+++ jims-main/src/main/java/com/jims/his/service/ieqm/ExpPrepareService.java
@@ -1,7 +1,16 @@
 package com.jims.his.service.ieqm;
 
+import com.jims.his.domain.ieqm.facade.ExpPrepareDetailFacade;
+import com.jims.his.domain.ieqm.vo.ExpPrepareVo;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/6/2.
@@ -9,5 +18,31 @@ import javax.ws.rs.Produces;
 @Path("exp-prepare")
 @Produces("application/json")
 public class ExpPrepareService {
+    private ExpPrepareDetailFacade expPrepareDetailFacade;
 
+    @Inject
+    public ExpPrepareService(ExpPrepareDetailFacade expPrepareDetailFacade){
+        this.expPrepareDetailFacade=expPrepareDetailFacade;
+    }
+
+    /**
+     * 根据条形码获取高值耗材信息
+     * */
+    @GET
+    @Path("find-by-bar-code")
+    public Map<String,Object> findByBarCode(@QueryParam("barCode") String barCode){
+        Map<String,Object> map=new HashMap<String,Object>();
+        List <ExpPrepareVo> list= expPrepareDetailFacade.findByBarCode(barCode);
+        if(list!=null&&list.size()>0){
+            map.put("info",list);
+         }else{
+            if(barCode!=null&&!"".equals(barCode)){
+                map.put("info","找不到对应的消耗品，barCode错误！");
+
+            }else{
+                map.put("info","barCode为空！");
+            }
+        }
+        return map;
+     }
 }
