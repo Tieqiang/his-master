04f01a2c9a620391e24fe5708655b178abeb5e03
chenxy <15101128707@163.com>
2016-06-16 15:46:13 +0800

    [cxy]11

--- jims-main/src/main/webapp/javascript/ieqm/exp-sub-import-detail.js
+++ jims-main/src/main/webapp/javascript/ieqm/exp-sub-import-detail.js
@@ -228,17 +228,18 @@ $(function () {
             field: 'tradePrice',
             width: "10%"
         }, {
-            title: '进价金额',
-            field: 'payAmount',
-            align: 'center',
-            width: "10%"
-        }, {
             title: '零售价金额',
             field: 'purchaseAmount',
             align: 'center',
             width: "10%"
         }, {
-            title: '零售价金额',
+            title: '进价金额',
+            field: 'purchaseAmount',
+            align: 'center',
+            hidden:true,
+            width: "10%"
+        }, {
+            title: '进价金额',
             field: 'payAmount',
             width: "10%"
         }, {
