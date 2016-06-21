7061b2a07b522f0b73f6796c24406840edbcec1d
zhu9644 <964483087@qq.com>
2016-06-21 16:52:27 +0800

    [朱齐]报表修改

--- jims-main/src/main/webapp/javascript/exp-class-summarizing.js
+++ jims-main/src/main/webapp/javascript/exp-class-summarizing.js
@@ -5,5 +5,6 @@
  * 按开支类别汇总
  */
 $(function(){
-    $("#report").prop("src",parent.config.defaultReportPath + "exp_class_summarizing.cpt&stockCode="+parent.config.storageCode);
+    var https="http://"+parent.config.reportDict.ip+":"+parent.config.reportDict.port+"/report/ReportServer?reportlet=exp/exp-list/exp_class_summarizing.cpt&stockCode="+parent.config.storageCode;
+    $("#report").prop("src",cjkEncode(https));
 });
