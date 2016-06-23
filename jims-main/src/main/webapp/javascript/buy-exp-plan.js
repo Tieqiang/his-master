0cb19e831bc7377fec0eed3abc84aa9cf9298438
zhu9644 <964483087@qq.com>
2016-06-23 08:30:44 +0800

    [朱齐]报表修改

--- jims-main/src/main/webapp/javascript/buy-exp-plan.js
+++ jims-main/src/main/webapp/javascript/buy-exp-plan.js
@@ -641,6 +641,7 @@ $(function () {
 
             $.postJSON("/api/buy-exp-plan/save", expChangeVo, function (data) {
                 $.messager.alert("系统提示", "保存成功", "info");
+                buyIds='';
                 $("#right").datagrid('loadData', {total: 0, rows: []});
                 $.get("/api/buy-exp-plan/get-buy-id", function (data) {
                     newBuyId = data;
