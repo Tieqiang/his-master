14752aa17a98197428e6debe7417b4402b7c1f91
fyg408224345 <408224345@qq.com>
2016-09-23 17:14:11 +0800

    [冯宇光]：采购计划执行打印，页面传到报表hospitalId

--- jims-main/src/main/webapp/javascript/ieqm/buy-exp-plan-execute.js
+++ jims-main/src/main/webapp/javascript/ieqm/buy-exp-plan-execute.js
@@ -298,7 +298,7 @@ $(function () {
         modal: true,
         closed: true,
         onOpen: function () {
-            var https="http://"+parent.config.reportDict.ip+":"+parent.config.reportDict.port+"/report/ReportServer?reportlet=exp/exp-list/buy-exp-plan-execute.cpt"+"&buyId=" + buyIds;
+            var https="http://"+parent.config.reportDict.ip+":"+parent.config.reportDict.port+"/report/ReportServer?reportlet=exp/exp-list/buy-exp-plan-execute.cpt"+"&buyId=" + buyIds + "&hospitalId=" + parent.config.hospitalId;
             $("#report").prop("src",cjkEncode(https));
             //$("#report").prop("src",parent.config.defaultReportPath + "buy-exp-plan-execute.cpt");
         }
